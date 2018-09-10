37ab140dae406d594769e81fd48f7c2448b298f1
AgungEdiKuncoro <42725305+AgungEdiKuncoro@users.noreply.github.com>
2018-09-10 16:30:16 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
 <?php
+echo "NO.1 : <br><br>"; 
+error_reporting(0);
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$angka = 5;
+function piramid($a, $b){
+if ($a >= 1) {
+	if ($b >= 1) {
+	echo "* ";
+	$b--;
+piramid($a, $b);
+
+}else{
+	echo "<br>";
+	$a--;
+piramid($a, $a);
+	}
 }
-rekursif(5);
-?>
+}
+piramid ($angka, $angka);
+
+echo "===========================================<br>";
+echo "NO.2 : <br><br>";
+
+function angka($a, $b, $angka){
+if ($a <= $angka) {
+	if ($b <= $a) {
+	echo "$b ";
+	$b++;
+angka($a, $b, $angka);
+        
+}else{
+	echo "<br>";
+	$a++;
+angka($a, 1, $angka);
+		}
+}
+}
+angka (1, 1, $angka);
+ ?>
